c558e7f5eefa03b9c320b45e9d16bfbeec1de041
icethreaed <hio.icethread@gmail.com>
2017-03-19 12:50:04 +0900

    ゲームルールのページ実装

--- MinecraftCommandController/Contents/Minecraft/McGameRule.Designer.cs
+++ MinecraftCommandController/Contents/Minecraft/McGameRule.Designer.cs
@@ -28,26 +28,212 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.numericUpDown1);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.button2);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.comboBox1);
 			this.groupBox1.Location = new System.Drawing.Point(3, 3);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(514, 100);
+			this.groupBox1.Size = new System.Drawing.Size(514, 82);
 			this.groupBox1.TabIndex = 0;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "ルール変更";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 52);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(47, 12);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "設定値 :";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 23);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(53, 12);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "ルール名 :";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Enabled = false;
+			this.numericUpDown1.Location = new System.Drawing.Point(168, 50);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(50, 19);
+			this.numericUpDown1.TabIndex = 5;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Enabled = false;
+			this.radioButton2.Location = new System.Drawing.Point(114, 50);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(48, 16);
+			this.radioButton2.TabIndex = 4;
+			this.radioButton2.Tag = "false";
+			this.radioButton2.Text = "false";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Enabled = false;
+			this.radioButton1.Location = new System.Drawing.Point(65, 50);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(43, 16);
+			this.radioButton1.TabIndex = 3;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Tag = "true";
+			this.radioButton1.Text = "true";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(433, 47);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "実行";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(433, 18);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "確認";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(65, 20);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(180, 20);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.dataGridView1);
+			this.groupBox2.Location = new System.Drawing.Point(3, 91);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(514, 286);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "ルール一覧";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+			this.dataGridView1.Location = new System.Drawing.Point(6, 18);
+			this.dataGridView1.MultiSelect = false;
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersVisible = false;
+			this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+			this.dataGridView1.RowTemplate.Height = 21;
+			this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dataGridView1.Size = new System.Drawing.Size(502, 253);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Column1
+			// 
+			this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCellsExceptHeader;
+			this.Column1.HeaderText = "ルール名";
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+			this.Column1.Width = 5;
+			// 
+			// Column2
+			// 
+			this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Column2.HeaderText = "説明";
+			this.Column2.Name = "Column2";
+			this.Column2.ReadOnly = true;
+			this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+			// 
+			// Column3
+			// 
+			this.Column3.HeaderText = "デフォルト";
+			this.Column3.Name = "Column3";
+			this.Column3.ReadOnly = true;
+			this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+			this.Column3.Width = 55;
 			// 
 			// McGameRule
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
 			this.Margin = new System.Windows.Forms.Padding(0);
 			this.Name = "McGameRule";
 			this.Size = new System.Drawing.Size(520, 380);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -55,5 +241,18 @@
 		#endregion
 
 		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
 	}
 }
--- MinecraftCommandController/Contents/Minecraft/McGameRule.cs
+++ MinecraftCommandController/Contents/Minecraft/McGameRule.cs
@@ -1,4 +1,6 @@
-﻿using MinecraftCommandController.Base;
+﻿using System.Windows.Forms;
+
+using MinecraftCommandController.Base;
 using MinecraftCommandController.Daos;
 using MinecraftCommandController.Entities;
 
@@ -23,6 +25,32 @@ namespace MinecraftCommandController.Contents.Minecraft
 			GameRuleCollectionEt grc = new GameRuleCollectionEt();
 			GameRuleDao dao = new GameRuleDao();
 			grc = dao.LoadXml(@".\Resource\gamerules.xml");
+			// アイテムに追加
+			if (grc != null)
+			{
+				foreach (GameRuleEt rule in grc.list)
+				{
+					ComboBoxGameRuleItem item = new ComboBoxGameRuleItem(rule);
+					comboBox1.Items.Add(item);
+					dataGridView1.Rows.Add(item.entity.Name, item.entity.Description, item.entity.Default);
+				}
+			}
+		}
+
+		// 効果選択用のコンボボックスアイテム
+		public class ComboBoxGameRuleItem
+		{
+			public GameRuleEt entity;
+
+			public ComboBoxGameRuleItem(GameRuleEt et)
+			{
+				this.entity = et;
+			}
+
+			public override string ToString()
+			{
+				return entity.Name;
+			}
 		}
 
 		/* ***** 以下、デザイナからの半自動生成 ***** */
@@ -30,6 +58,92 @@ namespace MinecraftCommandController.Contents.Minecraft
 		public McGameRule()
 		{
 			InitializeComponent();
+		}
+
+		// 選択ルールによって入力を分ける
+		private void comboBox1_SelectedValueChanged(object sender, System.EventArgs e)
+		{
+			ComboBoxGameRuleItem item = (ComboBoxGameRuleItem)comboBox1.SelectedItem;
+			switch (item.entity.Type)
+			{
+				case 1:
+					radioButton1.Enabled = true;
+					radioButton2.Enabled = true;
+					numericUpDown1.Enabled = false;
+					break;
+				case 2:
+					radioButton1.Enabled = false;
+					radioButton2.Enabled = false;
+					numericUpDown1.Enabled = true;
+					break;
+				default:
+					radioButton1.Enabled = false;
+					radioButton2.Enabled = false;
+					numericUpDown1.Enabled = false;
+					break;
+			}
+		}
+
+		// 確認ボタン
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			// ルール
+			ComboBoxGameRuleItem item = (ComboBoxGameRuleItem)comboBox1.SelectedItem;
+			if (item == null)
+			{
+				MessageBox.Show("ルールを選択してください。");
+				return;
+			}
+			string sRule = item.entity.Name.ToString();
+
+			// コマンド実行
+			string cmd = "";
+			cmd += "gamerule";
+			cmd += " " + sRule;
+			appMainForm.fncExecuteCommand(cmd);
+		}
+
+		// 実行ボタン
+		private void button2_Click(object sender, System.EventArgs e)
+		{
+			// ルール
+			ComboBoxGameRuleItem item = (ComboBoxGameRuleItem)comboBox1.SelectedItem;
+			if (item == null)
+			{
+				MessageBox.Show("ルールを選択してください。");
+				return;
+			}
+			string sRule = item.entity.Name.ToString();
+
+			// 設定値
+			string sValue = "";
+			switch (item.entity.Type)
+			{
+				case 1:
+					// ラジオボタンの値取得
+					foreach (Control ctrl in groupBox1.Controls)
+					{
+						if (ctrl is RadioButton && ((RadioButton)ctrl).Checked)
+						{
+							sValue = ctrl.Tag.ToString();
+						}
+					}
+					break;
+				case 2:
+					// 数値取得
+					sValue = numericUpDown1.Value.ToString();
+					break;
+				default:
+					MessageBox.Show("設定値が不明です。");
+					break;
+			}
+
+			// コマンド実行
+			string cmd = "";
+			cmd += "gamerule";
+			cmd += " " + sRule;
+			cmd += " " + sValue;
+			appMainForm.fncExecuteCommand(cmd);
 		}
 	}
 }
